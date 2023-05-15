28e64003f2c8b07582cdcdf90b68a022cae21bdd
Tev-J <tj_delph@yahoo.com>
2023-05-15 09:22:15 -0400

    rewrote structure for AquariumBuilder, added working eventlisteners to fishbuttons, completed CompatibilityGuide div

--- index.js
+++ index.js
@@ -5,48 +5,79 @@ document.addEventListener("DOMContentLoaded", function () {
     .then((data) => parseFishData(data));
 });
 
-let dataObj = [];
-
 function parseFishData(data) {
-  console.log(data);
-
   for (const item of data) {
-    buildSelectList(item);
-    showFishImages(item);
+    renderFishButtons(item);
+    renderFishImageScroller(item);
   }
 }
 
-//adds image of fish to fishPool
-function showFishImages(fish) {
-  const fishPoolContent = document.querySelector("#fishPool .content");
-
-  const image = document.createElement("img");
-  image.src = fish.img;
-  image.setAttribute("class", "image");
-
-  fishPoolContent.appendChild(image);
-}
-
-//creates selectButton for fish
-function buildSelectList(fish) {
+//creates button with an event listener for each fish
+function renderFishButtons(fish) {
   const header = document.getElementById("middleAreaH");
 
   const btn = document.createElement("button");
   btn.setAttribute("class", "fish-button");
   btn.textContent = `${fish.common_name}`;
+  btn.addEventListener("click", handlesClickForFishButtons);
   header.appendChild(btn);
-
   header.appendChild;
 }
 
-//handlesClickEvent -- populates side tab and header bar
-function handleImageClicks(e) {
-  console.log(e);
+//renders image for every fish inside of a scroller
+function renderFishImageScroller(fish) {
+  const scroller = document.querySelector("#fishPool .content");
+
+  const image = document.createElement("img");
+  image.src = fish.img;
+  image.setAttribute("class", "image");
+  scroller.appendChild(image);
 }
 
-//Event Listener for Buttons
-function fetchFromDB(web = "http://localhost:3000/fish") {
-  const fetchFishes = fetch(web)
+//handles the click event
+function handlesClickForFishButtons(e) {
+  const name = e.target.textContent;
+  buildGuide(name);
+  selectImage(name);
+}
+
+//fetches object with matching common_name
+function fetchFish(name) {
+  return fetch("http://localhost:3000/fish")
     .then((res) => res.json())
-    .then((data) => data);
+    .then((data) => data.find((elem) => elem.common_name === name));
+}
+
+//populates the compatibility guide with relative info from DB
+function buildGuide(fishName) {
+  fetchFish(fishName).then((fish) => {
+    const h2 = document.getElementById("title");
+    h2.textContent = `${fish.common_name}`;
+
+    const scientific_name = document.getElementById("ScientificName");
+    scientific_name.textContent = `Scientific Name: ${fish.scientific_name}`;
+
+    const behavior = document.getElementById("Behavior");
+    behavior.textContent = `Typical Behavior: ${fish.behavior}`;
+
+    const length = document.getElementById("Length");
+    length.textContent = `Max Length: ${fish.longest_average_length_inches} inches`;
+
+    const water_con = document.getElementById("Water_Con");
+    water_con.textContent = ` Water Condition Ranges - Temp: ${fish.water_conditions.temperature} | pH: ${fish.water_conditions.pH} | Water Hardness: ${fish.water_conditions.water_hardness}`;
+  });
+}
+
+// Removes the initialized scroller and fixes to image of fish
+function selectImage(fishName) {
+  fetchFish(fishName).then((fish) => {
+    const scroller = document.getElementById("scroller");
+    while (scroller.firstChild) {
+      scroller.removeChild(scroller.firstChild);
+    }
+
+    const img = document.createElement("img");
+    img.src = fish.img;
+    scroller.appendChild(img);
+  });
 }

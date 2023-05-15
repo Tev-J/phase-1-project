23f9eeb923e504374b84528f3b26d5d3b5b41aa5
Tev-J <tj_delph@yahoo.com>
2023-05-14 22:00:19 -0400

    switched buttons from footer to header, added pageInfo section, edited buttons

--- index.js
+++ index.js
@@ -1,29 +1,23 @@
 document.addEventListener("DOMContentLoaded", function () {
-  //
-  //
-  //
-  //API Endpoint
+  //API Endpoint to
   const fetchFishes = fetch("http://localhost:3000/fish")
     .then((res) => res.json())
     .then((data) => parseFishData(data));
-
-  //Event listener for images
-  const fishImages = document.querySelectorAll(".image");
-  console.log(fishImages);
-  fishImages.forEach((image) => {
-    image.addEventListener("click", handleImageClicks());
-  });
 });
 
+let dataObj = [];
+
 function parseFishData(data) {
+  console.log(data);
+
   for (const item of data) {
     buildSelectList(item);
-    buildFishProfile(item);
+    showFishImages(item);
   }
 }
 
 //adds image of fish to fishPool
-function buildFishProfile(fish) {
+function showFishImages(fish) {
   const fishPoolContent = document.querySelector("#fishPool .content");
 
   const image = document.createElement("img");
@@ -35,18 +29,24 @@ function buildFishProfile(fish) {
 
 //creates selectButton for fish
 function buildSelectList(fish) {
-  const footer = document.getElementById("footer");
+  const header = document.getElementById("middleAreaH");
 
   const btn = document.createElement("button");
   btn.setAttribute("class", "fish-button");
   btn.textContent = `${fish.common_name}`;
-  footer.appendChild(btn);
+  header.appendChild(btn);
 
-  footer.appendChild;
+  header.appendChild;
 }
 
 //handlesClickEvent -- populates side tab and header bar
 function handleImageClicks(e) {
-  console.log(e.target);
-  return e;
+  console.log(e);
+}
+
+//Event Listener for Buttons
+function fetchFromDB(web = "http://localhost:3000/fish") {
+  const fetchFishes = fetch(web)
+    .then((res) => res.json())
+    .then((data) => data);
 }
